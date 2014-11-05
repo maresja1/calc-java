53e6c5b362b87720b9f4bcb867439ec364ebcee5
Jan Mares <jan@mobikidsafe.com>
2014-11-05 09:48:29 +0100

    Fixing comments.

--- CodEx.java
+++ CodEx.java
@@ -4,28 +4,22 @@ import java.io.*;
 
 /** DU 1. !!! Nemente jmeno tridy !!!
   * 
-  * @author !!! DOPLN SVE JMENO !!!
+  * @author Jan Mares
   */
 public class CodEx {
 
-  public static void main(String[] argv) {
-    
-    /* VZOR: kod, ktery opisuje data ze standardniho vstupu na standardni vystup
-     * a konci pokud narazi na konec vstupu.
-     *
-     *
-     *  try {
-     *    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-     *    int c;
-     *    while ((c = input.read()) != -1) {
-     *      System.out.print((char) c);
-     *    }
-     *  } catch (IOException ex) {
-     *    System.out.println("Nastala IOException");
-     *  }
-     *
-     */
+    public static void main(String[] argv) {
 
-  }
+        try {
+            BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            int c;
+            while ((c = input.read()) != -1) {
+                System.out.print((char) c);
+            }
+        } catch (IOException ex) {
+            System.out.println("Nastala IOException");
+        }
+
+    }
 }
 
